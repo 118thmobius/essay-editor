0bbd522c7bd1234b36969476f27c7ab72ca666fd
mobius <mobius@mobiusone.org>
2025-10-16 22:49:52 +0900

    fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    proxy: {
-      '/api/grade': {
-        target: process.env.VITE_API_TARGET,
-        changeOrigin: true,
-        rewrite: () => '/grade',
-        secure: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    root: '.',
+    build: {
+      outDir: 'dist'
+    },
+    server: {
+      proxy: {
+        '/api/grade': {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+          rewrite: () => '/grade',
+          secure: true
+        }
       }
     }
-  }
+  };
 })
